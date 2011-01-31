21e2c7f5aabc628e62deaecefed90da9d45ab85b
Joe Goggins <goggins@umn.edu>
2011-01-31 14:17:57 -0600

    Removed those god-aweful mixin files, and merged them into a single file, for a 'simple' tool, that was a stupid thing to do...

--- lib/oracle_to_mysql.rb
+++ lib/oracle_to_mysql.rb
@@ -4,11 +4,11 @@
 #   and http://popen4.rubyforge.org/
 #
 require 'popen4'
-require 'oracle_to_mysql/protected_class_methods'
-require 'oracle_to_mysql/private_instance_methods'
-require 'oracle_to_mysql/must_override_instance_methods'
-require 'oracle_to_mysql/optional_override_instance_methods'
-require 'oracle_to_mysql/api_instance_methods'
+# require 'oracle_to_mysql/protected_class_methods'
+# require 'oracle_to_mysql/private_instance_methods'
+# require 'oracle_to_mysql/must_override_instance_methods'
+# require 'oracle_to_mysql/optional_override_instance_methods'
+# require 'oracle_to_mysql/api_instance_methods'
 
 # These commands are used internally to actuall do the work
 require 'oracle_to_mysql/command'
@@ -23,8 +23,286 @@ module OracleToMysql
   # used to join the table and timestamp for old retained tables if :n > 1, or if :n = 1 its simply the suffix of the table name
   OTM_RETAIN_KEY = '_old' 
   OTM_VALID_STRATEGIES = [:accumulative, :atomic_rename]
-
-
+  
+  module PrivateInstanceMethods
+    def otm_set_strategy(strategy,options={})
+      raise "Invalid strategy #{strategy}" unless OTM_VALID_STRATEGIES.include?(strategy)
+      @otm_strategy = strategy
+      # TODO: verify options
+      @otm_strategy_options = options
+    end
+    
+    def otm_started(msg)      
+      self.otm_output("[started t=#{self.otm_time_elapsed_since_otm_timestamp}]#{msg}")
+    end    
+    def otm_finished(msg)
+      self.otm_output("[finished t=#{self.otm_time_elapsed_since_otm_timestamp}]#{msg}")
+    end
+    # LOGGING FUNCTIONS END
+        
+    def otm_time_elapsed_since_otm_timestamp
+      if @otm_timestamp.nil?
+        self.otm_timestamp # calling this first, causes the lazily loaded timestamp to load...
+        0
+      else
+        Time.now - self.otm_timestamp
+      end
+    end
+    
+    # This reflects on the various strategy, retain, and other options that the client class has specified
+    # and generates a linear sequence of command names that will be executed
+    # on .otm_execute
+    #
+    def otm_execute_command_names
+      if @otm_execute_command_names.nil?
+        case self.otm_strategy
+        when :atomic_rename        
+          @otm_execute_command_names = [
+            :write_sqlplus_commands_to_file,
+            :fork_and_execute_sqlplus_commands_file,
+            :write_and_execute_mysql_commands_to_bash_file
+          ]
+        when :accumulative
+          @otm_execute_command_names = [
+            :write_sqlplus_commands_to_file,
+            :fork_and_execute_sqlplus_commands_file,
+            :write_and_execute_mysql_commands_to_bash_file_in_replace_mode
+          ]
+        else
+          raise "Invalid otm_strategy=#{@otm_strategy}"
+        end
+        
+        ## All strategies should cleanup after themselves lastly
+        @otm_execute_command_names << :delete_temp_files
+      end
+      @otm_execute_command_names                  
+    end         
+    
+    # This generates a temp file suffixed with the specified x
+    # it is used  
+    def generate_tmp_file_name(x)
+      File.join(self.tmp_directory,"#{self.otm_target_table}_#{self.otm_timestamp.to_i}_#{self.object_id}_#{Process.pid}_#{x}")
+    end
+    
+    # Must pass an argument, retain_n is "the number tables old relative to the current table"
+    # if n is 1, the retained table is simple <TBL>_old
+    # if its greater, there a dynamic "show retained tables for this table", sort lexically, 
+    # and return the -nth retain_n (i think) (negative nth) from the list
+    #
+    def otm_retained_target_table(retain_n)
+      if retain_n == 1
+        "#{self.otm_target_table}#{OTM_RETAIN_KEY}"
+      else
+        raise "TODO: HAVE NOT DEALT WITH n != 1 retain option"
+      end
+    end
+    
+  end
+  
+  module ProtectedClassMethods
+    def otm_default_post_mirror_options
+      {:optimize_table => true}
+    end    
+    def otm_default_strategy
+      :atomic_rename        # can also be :accumulative
+    end
+    
+    # These options set what happens to an existing target if it exists
+    # a retain value of n=1, means "keep the last table arround"
+    # 
+    def otm_default_retain_options
+      return {
+        :n => 1,
+        :table_name_pattern => Proc.new {|dest_table| Regexp.new(/(#{dest_table})(#{self.class::OTM_RETAIN_KEY})(\d+)/)},
+        :new_table_name => Proc.new {|dest_table| "#{dest_table}#{self.class::OTM_RETAIN_KEY}#{Time.now.to_i.to_s}"}
+      }        
+    end
+  end
+  
+  module OptionalOverrideInstanceMethods    
+    
+    # YOU WILL OFTEN WANT TO OVERRIDE THIS, 
+    def otm_strategy
+      if @otm_strategy.nil?
+        @otm_strategy = self.class.otm_default_strategy
+      end
+      @otm_strategy      
+    end
+              
+    # TO CHANGE the retain options, override this method
+    #    
+    def otm_retain_options
+      if @otm_retain_options.nil?
+        @otm_retain_options = self.class.otm_default_retain_options
+      end
+      @otm_retain_options
+    end
+    
+    # This results in mysql queries executing afterwords that optimize the table
+    # and get it set for prime time usage...
+    #
+    def otm_post_mirror_options
+      if @otm_post_mirror_options.nil?
+        @otm_post_mirror_options = self.class.otm_default_post_mirror_options
+      end
+      @otm_post_mirror_options      
+    end
+        
+    def otm_output(msg)
+      puts "[#{self.to_s}]#{msg}"
+    end        
+    
+    def otm_source_config_hash
+      if self.otm_config_hash.has_key?('oracle_source')
+        return otm_config_hash['oracle_source']
+      else
+        raise "Could not find oracle_source key in config file #{self.otm_config_file}, you should override this method"
+      end
+    end
+    def otm_target_config_hash
+      if self.otm_config_hash.has_key?('mysql_target')
+        return otm_config_hash['mysql_target']
+      else
+        raise "Could not find mysql_target key in config file #{self.otm_config_file}, you should override this method"
+      end      
+    end
+    
+    # Override this if you have zany rules about where your config file is, there is a handy otm_config_hash method in the ApiInstanceMethods
+    # you can use to get stuff outta here
+    #
+    def otm_config_file
+      non_rails_path = File.join(Dir.pwd, 'oracle_to_mysql.yml')
+      if defined?(RAILS_ROOT)
+        rails_path = File.join(RAILS_ROOT,'config','database.yml')
+        if rails_path
+          @otm_db_config_yml_file_name = rails_path
+        else
+          raise "Weird, RAILS_ROOT detected but no databases.yml, something is amiss!"
+        end
+      elsif File.exists?(non_rails_path)
+        @otm_db_config_yml_file_name = non_rails_path
+      else
+        raise "ERROR: No otm config file found"
+      end
+    end  
+    
+    # This is the table name that is created first on the server for the :atomic_rename strategy
+    #
+    def otm_temp_target_table
+      "#{self.otm_target_table}_#{self.otm_timestamp.to_i}_temp"
+    end
+    
+    def tmp_directory
+      "/tmp"  
+    end                                       
+  end
+  
+  module MustOverrideInstanceMethods    
+    def otm_source_sql
+      raise "YOU MUST OVERRIDE THIS"
+    end
+    def otm_target_sql
+      raise "YOU MUST OVERRIDE THIS"
+    end
+    def otm_target_table
+      raise "YOU MUST OVERRIDE THIS"
+    end  
+  end
+  
+  
+  module ApiInstanceMethods
+    def otm_execute
+      self.otm_output("[started at #{self.otm_timestamp}]")
+      self.otm_started("#otm_execute (in #{self.otm_strategy.to_s} mode, retain_n = #{self.otm_retain_options[:n]}")
+      self.otm_execute_command_names.each do |command_name|
+        command = OracleToMysql.get_and_bind_command(command_name, self)
+        begin
+          command.execute
+        rescue OracleToMysql::CommandError => e
+          raise e           
+        rescue Exception => e
+          raise e
+        end        
+      end
+      self.otm_finished("#otm_execute")
+      final_time_elapsed = self.otm_time_elapsed_since_otm_timestamp
+      finished_at = self.otm_timestamp + final_time_elapsed
+      
+      self.otm_output("[finished at #{finished_at}]")
+      self.otm_output("[completed in #{final_time_elapsed} seconds]")
+      self.otm_reset_timestamp
+      return true
+    end
+    
+    # This is used extensively by commands to read and write the the specific files
+    # that are shared accross commands, it is a bit protected-ish though (aka prolly don't want to override this)
+    #
+    def otm_get_file_name_for(sym)
+      f = self.generate_tmp_file_name(sym)
+      case sym
+      when :mysql_commands, :oracle_commands
+        "#{f}.sql"   # sqlplus is such a jackass, the file name must end in .sql for it to work, fyi
+      when :oracle_output
+        "#{f}.txt"
+      else
+        raise "Invalid file_name #{sym}"
+      end
+    end
+    
+    # For interaction with the configuration file that contains the mysql or oracle stuff
+    # override otm_config_file if you have yer config somewhere crazy
+    #
+    def otm_config_hash
+      if @otm_confile_file_hash.nil?
+        @otm_confile_file_hash = YAML.load(File.read(self.otm_config_file))
+      end
+      @otm_confile_file_hash      
+    end
+    
+    # all tables and file name are versioned against the seconds of this Time obj
+    #
+    def otm_timestamp
+      if @otm_timestamp.nil?
+        @otm_timestamp = Time.now
+      end
+      @otm_timestamp
+    end
+    
+    # You need to do this to invoke .otm_execute twice on the same inst, or it will clobber
+    def otm_reset_timestamp
+      @otm_timestamp = nil # reset it so all temp files have a new identifier, if otm_execute'd is executed again
+    end
+    
+    # Returns all temp files that all commands in the current strategy can create
+    #
+    def otm_all_temp_files
+      return_this = []
+      self.otm_execute_command_names.each do |command_name|
+        command = OracleToMysql.get_and_bind_command(command_name, self)
+        return_this += command.temp_file_symbols.map {|sym| self.otm_get_file_name_for(sym)}
+      end
+      return_this.uniq
+    end
+        
+    # returns an array of all target tables by reflecting the table retention options
+    #
+    def otm_all_target_tables
+      return_this = [self.otm_target_table]
+      if self.otm_retain_options[:n] == 0
+        return_this
+      else
+        (1..self.otm_retain_options[:n]).to_enum.each do |x|
+          return_this << self.otm_retained_target_table(x)
+        end
+        return_this
+      end
+    end
+  end
+  
+  
+  
+  
+  
   # Stuff mixed into a client class calling include OracleToMysql
   def self.included(caller)
     caller.class_eval do
