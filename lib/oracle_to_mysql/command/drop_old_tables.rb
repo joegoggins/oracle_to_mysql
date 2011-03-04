021c32c856d1c30c9b5740381d5bee21293b147b
Joe Goggins <goggins@umn.edu>
2011-03-04 11:13:04 -0600

    Using the :offset option in the sql_for_old_tables functionality Added :socket connection signature to mysql gem usage

--- lib/oracle_to_mysql/command/drop_old_tables.rb
+++ lib/oracle_to_mysql/command/drop_old_tables.rb
@@ -3,26 +3,35 @@ module OracleToMysql
     class DropOldTables < OracleToMysql::Command
       def execute_internal
         h =self.client_class.otm_target_config_hash
-        conn = Mysql.new(
-          h['host'], # no host
-          h['username'],
-          h['password'],
-          h['database'],
-          h['port']
-        )
+        # Create MySQL connection via host or socket
+        if h.has_key?(:socket)
+          self.info("Connecting with mysql gem via socket")
+          conn = Mysql.new(
+            nil, # no host
+            h['username'],
+            h['password'],
+            h['database'],
+            nil, # no port
+            h[:socket]
+          )
+        else
+          self.info("Connecting with mysql gem via TCP")
+          conn = Mysql.new(
+            h['host'], 
+            h['username'],
+            h['password'],
+            h['database'],
+            h['port']
+          )
+        end
 
-        old_tables_results = conn.query(self.client_class.otm_table_namer.sql_for_old_tables(h['database']))
-        count = 0
+        old_tables_to_drop = conn.query(
+          self.client_class.otm_table_namer.sql_for_old_tables(h['database'], :offset => self.client_class.otm_number_of_tables_to_retain)
+        )
         self.info("Dropping old tables, will retain #{self.client_class.otm_number_of_tables_to_retain}")
-        while row = old_tables_results.fetch_hash do
-          if count < self.client_class.otm_number_of_tables_to_retain
-            self.info("Not dropping #{row['table_name']}")
-            # Do nothing, this table should be retained
-          else
-            conn.query("drop table #{row['table_name']}")  
-            self.info("Dropped #{row['table_name']}")
-          end
-          count += 1 
+        while row = old_tables_to_drop.fetch_hash do
+          conn.query("drop table #{row['table_name']}")  
+          self.info("Dropped #{row['table_name']}")
         end
       end
     end      
