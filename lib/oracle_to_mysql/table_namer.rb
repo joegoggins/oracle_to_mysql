51e64845e5513ea52b8d542ff0bd3185d0912be2
Ian Whitney <whit0694@umn.edu>
2015-04-02 18:38:17 -0500

    Standardize on [table_name]_old for archive tables
    Between the two versions of oracle_to_mysql (and some other code) we
    ended up with two ways of naming archive tables. Version one is
    
    [table_name]_old
    
    And version two is
    
    old_[date time]_[table_name]
    
    The goal of the 2nd form was to allow us to store multiple days of
    archives. But, as far as I can see, there is no actual use for multiple
    days of archives. They just take up space.
    
    And having table names in the 2nd form causes havoc for granting access.
    In MySQL you can grant a user access to a specific table or all tables.
    You can not grant them access to a table name with a wild card in it. So
    if, say, student athletes needs to select data from ps_person and
    compare it against yesterday's archive, then the first form makes this
    easy:
    
    grant select to 'ps_person'
    grant select to 'ps_person_old'
    
    But the 2nd form offers no easy way to do that. You can't do
    
    grant select to 'old_%_ps_person'
    
    So you either have to give the user select access to all tables. Or you
    have to programatically re-assign grants each night.
    
    If we were using the multi-day archive then this might (might) be worth
    the hassle. But we're not, so it's not, so I'm removing it.
    
    This commit will require that DW_Import_Scripts be updated as well,
    since it names the archive tables inconsistently.

--- lib/oracle_to_mysql/table_namer.rb
+++ lib/oracle_to_mysql/table_namer.rb
@@ -3,41 +3,32 @@ module OracleToMysql
     attr_accessor :table_name, :now
     def initialize(table_name,*args)
       options = args.first || {}
-      @table_name = table_name 
+      @table_name = table_name
       @now = options[:now] || Time.now
     end
 
     def yesterday
-      tt=(self.now - 24 * 60 * 60).strftime('%Y%m%d')
-      "old_#{tt}_#{self.table_name}"
+      "#{self.table_name}_old"
     end
+
     def temp
       tt=self.now.to_i
       "temp_#{tt}_#{self.table_name}"
-    end
-
-    def old_table_sql_like_wildcard
-      # When using a LIKE clause, we have to escape underscores or they'll be 
-      # interpreted as a single-character wildcard. The 8 unescaped underscores
-      # match the date in the table name. Using a '%' here can cause overlap
-      # can delete tables you want to keep!
-      # http://dev.mysql.com/doc/refman/5.0/en/string-comparison-functions.html#operator_like
-      "old\\_________\\_#{self.table_name.gsub('_', '\_')}"
     end
 
     # list old tables, newest first
     def sql_for_old_tables(schema, options={:offset => 0})
       # http://stackoverflow.com/questions/255517/mysql-offset-infinite-rows for limit statement details
       "
-      SELECT 
-         table_name 
-       FROM information_schema.tables 
-       WHERE 
-         table_name LIKE '#{old_table_sql_like_wildcard}' 
+      SELECT
+         table_name
+       FROM information_schema.tables
+       WHERE
+         table_name LIKE '#{yesterday}'
          AND table_schema = '#{schema}'
        ORDER BY table_name DESC
        LIMIT #{options[:offset]}, 18446744073709551615
       "
-    end 
+    end
   end
 end
